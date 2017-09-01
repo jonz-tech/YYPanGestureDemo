c3598cdef8f05e18c47cecd477f922009f63d728
jonz-tech <jonz-tech@users.noreply.github.com>
2017-09-01 21:09:36 +0800

    Initial Commit

---
+++ YYPanGestureDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYPanGestureDemo
+//
+//  Created by zhuangchubin on 2017/9/1.
+//  Copyright © 2017年 zhuangchubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
